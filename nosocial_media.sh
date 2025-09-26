77daeae2f1b86059db3cb1e1897392b6da52543e
dimadeloseros1 <dimadeloseros1@gmail.com>
2025-09-26 20:34:19 +1000

    Feat: Implemenet unblock websites

--- nosocial_media.sh
+++ nosocial_media.sh
@@ -1,20 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# Sites to block
 social_media=(
-    twitter.com
-    www.twitter.com
-    api.twitter.com
-    youtube.com
-    www.youtube.com
-    x.com
-    www.x.com
-    reddit.com
-    www.reddit.com
+  twitter.com
+  www.twitter.com
+  api.twitter.com
+  youtube.com
+  www.youtube.com
+  x.com
+  www.x.com
+  reddit.com
+  www.reddit.com
 )
 
-# Windows hosts file location
-HOSTS_FILE="/c/Windows/System32/drivers/etc/hosts"
 
-for i in "${social_media[@]}"; do
-    echo "127.0.0.1 $i" | tee -a "$HOSTS_FILE" > /dev/null
-done
+if [[ $1 == "block" ]]; then
+	for i in "${social_media[@]}"; do
+		sed -i "/^127.0.0.1 $i/d" /c/Windows/System32/drivers/etc/hosts
+		echo "127.0.0.1 $i" >> /c/Windows/System32/drivers/etc/hosts
+	done
+fi
+
+if [[ $1 == "unblock" ]]; then
+	for i in "${social_media[@]}"; do
+		sed -i "/^127.0.0.1 $i/d" /c/Windows/System32/drivers/etc/hosts
+	done
+fi
+
