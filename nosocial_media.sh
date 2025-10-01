9734bd5eb8a284428505b5b74af414c4889fb662
dimadeloseros1 <dimadeloseros1@gmail.com>
2025-10-01 15:34:00 +1000

    Feat: Implement code readability

--- nosocial_media.sh
+++ nosocial_media.sh
@@ -1,27 +1,44 @@
-#!/usr/bin/env bash
 
-# Sites to block
-social_media=(
-  twitter.com
-  www.twitter.com
-  api.twitter.com
-  youtube.com
-  www.youtube.com
-  x.com
-  www.x.com
-  reddit.com
-  www.reddit.com
-)
+#!/bin/bash
 
-if [[ $1 == "block" ]]; then
-	for i in "${social_media[@]}"; do
-		sed -i "/^127.0.0.1 $i/d" /c/Windows/System32/drivers/etc/hosts
-		echo "127.0.0.1 $i" >> /c/Windows/System32/drivers/etc/hosts
-	done
-fi
+# Path to hosts file 
+HOSTS="/c/Windows/System32/drivers/etc/hosts"
 
-if [[ $1 == "unblock" ]]; then
-	for i in "${social_media[@]}"; do
-		sed -i "/^127.0.0.1 $i/d" /c/Windows/System32/drivers/etc/hosts
-	done
-fi
+# Social media sites to block
+SITES="twitter.com www.twitter.com api.twitter.com youtube.com www.youtube.com x.com www.x.com reddit.com www.reddit.com"
+
+blocks() {
+    echo "Blocking social media sites..."
+    for site in $SITES; do
+        # Check if already blocked
+        if ! grep -q "127.0.0.1 $site" "$HOSTS" 2>/dev/null; then
+            echo "127.0.0.1 $site" >> "$HOSTS"
+            echo "Blocked: $site"
+        else
+            echo "Already blocked: $site"
+        fi
+    done
+    echo "Done blocking!"
+}
+
+unblocks() {
+    echo "Unblocking social media sites..."
+    for site in $SITES; do
+        sed -i "/127.0.0.1 $site/d" "$HOSTS"
+        echo "Unblocked: $site"
+    done
+    echo "Done unblocking!"
+}
+
+case "$1" in
+    block)
+        blocks
+        ;;
+    unblock)
+        unblocks
+        ;;
+    *)
+        echo "Usage: $0 block | unblock"
+        exit 1
+        ;;
+esac
